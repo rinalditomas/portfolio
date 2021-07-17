a658701958153e48a3a4fca65071e4688ca542ac
rinalditomas <rinalditomas@gmail.com>
2021-07-18 00:23:18 +0100

    some changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
-            <span>© 2021 Created by <a href="https://www.linkedin.com/in/tomas-rinaldi-b4745b86/">Tomas Rinaldi</a> </span>
+            <span style={{color:'black'}}>© 2021 Created by <a href="https://www.linkedin.com/in/tomas-rinaldi-b4745b86/">Tomas Rinaldi</a> </span>
         </div>
     )
 }
