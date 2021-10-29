5f284e51c9097360586a5e9e94526a3ef3a005c2
rinalditomas <rinalditomas@gmail.com>
2021-10-29 13:06:40 +0100

    fixing footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { TEXT } from "../../constants/ProjectList";
-import './Footer.css'
 export default function Footer() {
-  const {title, name} = TEXT.FOOTER
+  const { title, name } = TEXT.FOOTER;
   return (
-    <div className="footer">
+    <div
+      className="footer"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontWeight: 400,
+        fontSize: "0.8em",
+        marginTop: "2rem",
+        height: "5rem",
+      }}
+    >
       <span style={{ color: "black" }}>
         {title}{" "}
-        <a href="https://www.linkedin.com/in/tomas-rinaldi-b4745b86/">
-         {name}
-        </a>
+        <a href="https://www.linkedin.com/in/tomas-rinaldi-b4745b86/">{name}</a>
       </span>
     </div>
   );
