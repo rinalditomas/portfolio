eec7011d37bdb352e5c8539eced66bd0130f045b
rinalditomas <rinalditomas@gmail.com>
2021-10-29 13:35:15 +0100

    trying to fix netlify problem

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TEXT } from "../../constants/ProjectList";
-import '../Footer/Footer.css'
+import '../../../src/components/Footer/Footer.css'
 
 
 export default function Footer() {
