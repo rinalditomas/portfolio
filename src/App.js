bf92398fdf66ad064c2ed25974fc51d745b0d7d7
rinalditomas <rinalditomas@gmail.com>
2021-10-29 13:21:03 +0100

    fixing footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
